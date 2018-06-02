e941357c1ba538008c73641d918928896f4849f5
Piotr <piwpio@gmail.com>
2018-06-02 15:15:05 +0200

    move right model

---
+++ Models/MoveRightModel.js
@@ -0,0 +1,25 @@
+var execute = function(hero)
+{
+    var locationId = hero.getLocation();
+    var Locations = module.parent.parent.exports.Locations;
+    var currentLocation = Locations.getLocation(locationId);
+    if (currentLocation.canGoRight()) {
+        var newLocationId = currentLocation.getRightLocationId();
+        var newLocation = Locations.getLocation(newLocationId);
+        hero.setLocation(newLocationId);
+        //send heroes on current location info, that hero just moved from this location
+        currentLocation.removeHeroFromLocation(hero);
+        //send heroes on new location info, that hero just moved to this location
+        newLocation.addHeroToLocation(hero);
+
+    } else {
+        hero.emitError({message: 'cant go right'});
+        return;
+    }
+
+    hero.sendResponse();
+};
+
+module.exports = {
+    execute
+};
